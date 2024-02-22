1161aa46aeae54f72b473a201b129aaf06cb6926
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2024-02-22 14:30:03 -0800

    feat: add Snapshot commands

---
+++ src/commands/feeds/snapshot/add-spaces.ts
@@ -0,0 +1,77 @@
+import { ChatInputCommandInteraction, TextChannel } from "discord.js";
+
+import FeedConfig from "../../../database/FeedConfig";
+import { NermanClient } from "../../../types";
+import SnapshotSpace from "../../../database/SnapshotSpace";
+import { Types } from "mongoose";
+
+export default {
+	subCommand: "snapshot.add-spaces",
+
+	async execute(interaction: ChatInputCommandInteraction) {
+		console.log("commands/feeds/snapshot: Adding new spaces.", {
+			userId: interaction.user.id,
+			guildId: interaction.guildId,
+			channelId: interaction.channelId
+		});
+
+		const channel = interaction.channel;
+		const spaceIdsOptions = interaction.options.getString("space-ids");
+
+		if (!channel) {
+			throw new Error("No channel.");
+		}
+		if (!spaceIdsOptions) {
+			throw new Error("No space ids.");
+		}
+
+		const spaceIds = spaceIdsOptions.split(",").map((spaceId) => spaceId.trim());
+		const snapshot = (interaction.client as NermanClient).libraries.snapshot;
+
+		// Optimize the below to only do the I/O once on the whole list.
+		for (const spaceId of spaceIds) {
+			const isAlreadyListenedTo = snapshot.spaceIds.includes(spaceId);
+			if (!isAlreadyListenedTo) {
+				snapshot.addSpace(spaceId);
+
+				SnapshotSpace.create({
+					_id: new Types.ObjectId(),
+					spaceId: spaceId
+				})
+					.then(() => {
+						console.log("commands/feeds/snapshot: Registered new snapshot space.", {
+							spaceId
+						});
+					})
+					.catch((error: any) => {
+						console.error("commands/feeds/snapshot: Unable to register new snapshot space.", {
+							spaceId,
+							error
+						});
+					});
+			}
+
+			const feeds = await FeedConfig.find({
+				channelId: channel.id,
+				guildId: interaction.guildId,
+				eventName: { $regex: /snapshot/, $options: "gi" } // Finds all snapshot events.
+			}).exec();
+
+			feeds.forEach((feed) => {
+				feed.options?.snapshot?.spaces.push({ id: spaceId, url: "https://snapshot.org/#/" + spaceId });
+				feed.save();
+			});
+		}
+
+		await interaction.reply({
+			ephemeral: true,
+			content: `${spaceIdsOptions} added!`
+		});
+
+		console.log("commands/feeds/snapshot: Finished adding new spaces.", {
+			userId: interaction.user.id,
+			guildId: interaction.guildId,
+			channelId: interaction.channelId
+		});
+	}
+};
