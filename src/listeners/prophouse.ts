713097a35806bcfd7ea2cfd3b2b652c5f6a72bec
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2024-02-23 13:08:09 -0800

    feat: filter Prophouse feeds by houses.

--- src/listeners/prophouse.ts
+++ src/listeners/prophouse.ts
@@ -1,6 +1,7 @@
 import { NermanClient, Events } from "../types";
 import Proposal from "../database/Proposal";
 import * as embeds from "../embeds/prophouse";
+import FeedConfig from "../database/FeedConfig";
 
 export default async function listenToProphouseEvents(client: NermanClient) {
 	const { prophouse, router, ensCache } = client.libraries;
@@ -18,7 +19,20 @@ export default async function listenToProphouseEvents(client: NermanClient) {
 		};
 
 		console.log("listeners/prophouse: RoundCreated embed data.", { ...embedData });
-		router.forEachFeedChannel("prop-house-round-created", (channel) => {
+		router.forEachFeedChannel("prop-house-round-created", async (channel) => {
+			try {
+				const feedConfig = await FeedConfig.findOne({
+					channelId: channel.id,
+					guildId: channel.guildId,
+					eventName: "prop-house-round-created"
+				}).exec();
+				if (!feedConfig?.includesHouse(embedData.house.id)) {
+					return;
+				}
+			} catch (error) {
+				console.error("listeners/prophouse: Unable to retrieve FeedConfig for Prophouse.", error);
+			}
+
 			const embed = embeds.generateRoundCreatedEmbed(embedData);
 			channel.send({ embeds: [embed] });
 		});
@@ -37,7 +51,20 @@ export default async function listenToProphouseEvents(client: NermanClient) {
 		};
 
 		console.log("listeners/prophouse: HouseCreated embed data.", { ...embedData });
-		router.forEachFeedChannel("prop-house-house-created", (channel) => {
+		router.forEachFeedChannel("prop-house-house-created", async (channel) => {
+			try {
+				const feedConfig = await FeedConfig.findOne({
+					channelId: channel.id,
+					guildId: channel.guildId,
+					eventName: "prop-house-house-created"
+				}).exec();
+				if (!feedConfig?.includesHouse(embedData.house.id)) {
+					return;
+				}
+			} catch (error) {
+				console.error("listeners/prophouse: Unable to retrieve FeedConfig for Prophouse.", error);
+			}
+
 			const embed = embeds.generateHouseCreatedEmbed(embedData);
 			channel.send({ embeds: [embed] });
 		});
@@ -54,7 +81,20 @@ export default async function listenToProphouseEvents(client: NermanClient) {
 		};
 
 		console.log("listeners/prophouse: VoteCast embed data.", { ...embedData });
-		router.forEachFeedChannel("prop-house-vote-cast", (channel) => {
+		router.forEachFeedChannel("prop-house-vote-cast", async (channel) => {
+			try {
+				const feedConfig = await FeedConfig.findOne({
+					channelId: channel.id,
+					guildId: channel.guildId,
+					eventName: "prop-house-vote-cast"
+				}).exec();
+				if (!feedConfig?.includesHouse(embedData.house.id)) {
+					return;
+				}
+			} catch (error) {
+				console.error("listeners/prophouse: Unable to retrieve FeedConfig for Prophouse.", error);
+			}
+
 			const embed = embeds.generateVoteCastEmbed(embedData);
 			channel.send({ embeds: [embed] });
 		});
@@ -72,7 +112,20 @@ export default async function listenToProphouseEvents(client: NermanClient) {
 		};
 
 		console.log("listeners/prophouse: ProposalSubmitted embed data.", { ...embedData });
-		router.forEachFeedChannel("prop-house-proposal-submitted", (channel) => {
+		router.forEachFeedChannel("prop-house-proposal-submitted", async (channel) => {
+			try {
+				const feedConfig = await FeedConfig.findOne({
+					channelId: channel.id,
+					guildId: channel.guildId,
+					eventName: "prop-house-proposal-submitted"
+				}).exec();
+				if (!feedConfig?.includesHouse(embedData.house.id)) {
+					return;
+				}
+			} catch (error) {
+				console.error("listeners/prophouse: Unable to retrieve FeedConfig for Prophouse.", error);
+			}
+
 			const embed = embeds.generateProposalSubmittedEmbed(embedData);
 			channel.send({ embeds: [embed] });
 		});
